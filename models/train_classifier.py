48431d084e7dc77c60572c6617672b5120cc62a1
Terry Lu <u0509421@gmail.com>
2019-01-07 21:00:31 +0800

    Update train_classifier.py
    add description to functions.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -27,14 +27,14 @@ nltk.download(['punkt','stopwords','wordnet'])
 def load_data(database_filepath):
     """
     Load datasets from local SQLite database
-    
+
     Args:
     database_filename: string. Filename for SQLite database containing cleaned message data.
-       
+
     Returns:
     X: dataframe. Dataframe containing features dataset.
     y: dataframe. Dataframe containing labels dataset.
-    col_names: List of strings. List containing category names. 
+    col_names: List of strings. List containing category names.
     """
     # load data from database
     engine = create_engine('sqlite:///' + database_filepath)
@@ -46,22 +46,22 @@ def load_data(database_filepath):
     X = df['message']
     y = df.iloc[:,4:].astype(int)
     col_names = y.columns.values
-    
+
     return X, y, col_names
 
 
 def tokenize(text):
     """
     Normalize, tokenize and lemmatize text string
-    
+
     Args:
     text: string. String containing message for processing
-       
+
     Returns:
     clean_tokens: list of strings. List containing normalized and lemmatized word tokens
     """
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
-    
+
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -75,12 +75,12 @@ def tokenize(text):
 def build_model():
     """
     Build a machine learning pipeline
-    
+
     Args:
     None
-       
+
     Returns:
-    cv: gridsearchcv object. Gridsearchcv object that transforms the data, creates the 
+    cv: gridsearchcv object. Gridsearchcv object that transforms the data, creates the
     model object and finds the optimal model parameters.
     """
     pipeline = Pipeline([
@@ -88,28 +88,29 @@ def build_model():
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(RandomForestClassifier()))
     ])
-    
+
     parameters = {
         'vect__min_df': [1, 5],
         'tfidf__use_idf':[True, False],
-        'clf__estimator__n_estimators':[10, 25], 
+        'clf__estimator__n_estimators':[10, 25],
         'clf__estimator__min_samples_split':[2, 5, 10]
     }
 
     cv = GridSearchCV(pipeline, param_grid=parameters)
-    
+
     return cv
 
 def get_evaluation(y_test, y_pred, category_names):
     """
     Evaluate model performance based on accuracy, precision, recall, F1 score
-    
+
     Args:
     y_test: array. Actual labels.
     y_pred: array. Predicted labels.
-       
+    category_names: list of strings. List containing names for each of the predicted fields.
+
     Returns:
-    metrics_df: dataframe. Dataframe containing the accuracy, precision, recall 
+    metrics_df: dataframe. Dataframe containing the accuracy, precision, recall
     and f1 score for a given set of actual and predicted labels.
     """
     metrics = []
@@ -118,30 +119,30 @@ def get_evaluation(y_test, y_pred, category_names):
         precision = precision_score(y_test[:,i], y_pred[:,i], average='micro')
         recall = recall_score(y_test[:,i], y_pred[:,i], average='micro')
         f1 = f1_score(y_test[:,i], y_pred[:,i], average='micro')
-        
+
         metrics.append([accuracy, precision, recall, f1])
-        
+
     df = pd.DataFrame(data = np.array(metrics), index=category_names, columns=['Accuracy', 'Precision', 'Recall', 'F1 score'])
-    
+
     return df
 
 def evaluate_model(model, X_test, Y_test, category_names):
     """
     Evaluate model performance based on accuracy, precision, recall, F1 score
-    
+
     Args:
     actual: array. Array containing actual labels.
     predicted: array. Array containing predicted labels.
-    col_names: list of strings. List containing names for each of the predicted fields.
-       
+    category_names: list of strings. List containing names for each of the predicted fields.
+
     Returns:
-    metrics_df: dataframe. Dataframe containing the accuracy, precision, recall 
+    metrics_df: dataframe. Dataframe containing the accuracy, precision, recall
     and f1 score for a given set of actual and predicted labels.
     """
     Y_pred = model.predict(X_test)
-    
+
     print(get_evaluation(np.array(Y_test), Y_pred, category_names))
-    
+
 
 def save_model(model, model_filepath):
     with open(model_filepath, 'wb') as f:
@@ -154,13 +155,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
